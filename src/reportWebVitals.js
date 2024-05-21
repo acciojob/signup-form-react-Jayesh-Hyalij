52a633112873388c0befbba0ef56e4126892e8ff
Jayesh Hyalij <115018197+Jayesh-Hyalij@users.noreply.github.com>
2024-05-21 18:38:44 +0530

    Create reportWebVitals.js

---
+++ src/reportWebVitals.js
@@ -0,0 +1,13 @@
+// src/reportWebVitals.js
+import { getCLS, getFID, getLCP, getTTFB } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
+  }
+};
+
+export default reportWebVitals;
